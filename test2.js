f462510594907342d4a2831fa6dddab03fd039a7
Abanoub <Abanoub@test.com>
2024-12-07 19:52:29 +0200

    First commit

---
+++ test2.js
@@ -0,0 +1,23 @@
+let but = document.querySelector('.icon');
+let but1 = document.querySelector('.icon1');
+let password = document.querySelector('.password');
+but.classList.add('hide')
+but.onclick = function (event) {
+    but.classList.add('hide');
+    but1.classList.remove('hide');
+    if (password.type === "password") {
+        password.type = "text";
+    } else {
+        password.type = "password";
+    }
+    event.preventDefault();};
+
+but1.onclick = function (event) {
+    but1.classList.add('hide');
+    but.classList.remove('hide');
+    if (password.type === "text") {
+        password.type = "password";
+    } else {
+        password.type = "text";
+    }
+event.preventDefault();};
